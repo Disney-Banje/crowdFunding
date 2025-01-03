73fc19333b1edf6bebf84dd137835dedd4ef92df
Disney <disneyconstant996@gmail.com>
2025-01-03 17:30:49 +0300

    Add functionality for radio buttons

--- main.js
+++ main.js
@@ -4,17 +4,47 @@ import { PledgeCard } from "./modules/label.js";
 import { getPledgeData } from "./modules/getData.js";
 
 const backProject = document.querySelector('.cta-btn');
-const app = document.querySelector('#app');
+export const app = document.querySelector('#app');
 const main = document.querySelector('main');
+const rewardButton = document.querySelectorAll('.reward-cta');
 
 document.addEventListener('DOMContentLoaded', async () => {
     const generate = await createModalDefault();
 
+    function applyFilter() {
+        app.style.filter = 'brightness(50%)';
+    }
+
+    const popup = createCompletedMessage();
+
     backProject.addEventListener('click', () => {
         document.body.appendChild(generate);
-        app.style.filter = 'brightness(50%)';
+        applyFilter();
+
+        const radioButtons = document.querySelectorAll('input[name="pledges"]');
+        const pledgeArticles = document.querySelectorAll('.pledge-article');
+
+        radioButtons.forEach((radioButton, index) => {
+            radioButton.addEventListener('change', () => {
+                pledgeArticles.forEach(pledge => {
+                    pledge.classList.remove('selected');
+                })
+                if (radioButton.checked) {
+                    pledgeArticles[index].classList.toggle('selected');
+                }
+            })
+        });
     });
 
+    rewardButton.forEach(reward => {
+        reward.addEventListener("click", () => {
+            document.body.appendChild(popup);
+            applyFilter();
+        })
+    })
+
     // app.appendChild(generate);
-    console.log(generate);
+    
+    // app.appendChild(popup);
+    // console.log(generate);
 })
--- modules/completed.js
+++ modules/completed.js
@@ -1,9 +1,10 @@
+import { app } from "../main.js";
 export function createCompletedMessage() {
     const article = document.createElement('article');
     article.classList.add('completed-wrapper');
 
     const icon = document.createElement('img');
-    icon.src = '../images/icon-bookmark.svg'
+    icon.src = '../images/icon-check.svg'
 
     const heading2 = document.createElement('h2');
     heading2.textContent = `Thanks for your support!`;
@@ -12,10 +13,11 @@ export function createCompletedMessage() {
     para.textContent = `Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Rise worldwide. You will get an email once our campaign is completed.`;
 
     const confirmBtn = document.createElement('button');
-    confirmBtn.classList.add('confirm-btn');
+    confirmBtn.classList.add('cta', 'confirm-btn');
     confirmBtn.textContent = 'Got it!';
     confirmBtn.onclick = () => {
         article.style.display = 'none';
+        app.style.filter = 'none';
     }
 
 
